395e670f840d42d46dc91581ddb248fa46b96a1e
chaitanyamehta1 <mehtachaitanya1179@gmail.com>
2020-05-20 15:34:07 +0100

    studentapp-serenity-project

---
+++ IdeaProjects/API/homework/studentapp-serenity-project/src/test/java/com/lc/df/studentapp/testbase/TestBase.java
@@ -0,0 +1,17 @@
+package com.lc.df.studentapp.testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by Chaitanya
+ */
+public class TestBase {
+
+    @BeforeClass
+    public static void init(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;
+        RestAssured.basePath = "/student";
+    }
+}
